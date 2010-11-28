429d415bc9ae449b4e6f0f5d12b141e3f16d8f24
santiago.hollmann <santiago.hollmann@c02190c7-70d5-933a-f1a5-febd6bb59b4f>
2010-11-28 18:07:11 +0000

    Pantallas

---
+++ Normalizator/src/ar/com/nn/view/CargaAtributosWindow.java
@@ -0,0 +1,147 @@
+package ar.com.nn.view;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.List;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import ar.com.nn.busisness.Relacion;
+
+public class CargaAtributosWindow {
+
+	private JFrame contenedor;
+	private JTextField txtfieldCargarAtributos;
+	private List listaAtributos;
+	private JButton btnBorrar;
+	private ArrayList<String> atributosRelacion;
+	private String atributo;
+	private static CargaAtributosWindow INSTANCE = null;
+
+	// creador sincronizado para protegerse de posibles problemas multi-hilo
+	// otra prueba para evitar instanciaci—n mœltiple
+	private synchronized static void createInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new CargaAtributosWindow();
+		}
+	}
+
+	public static CargaAtributosWindow getInstance() {
+		if (INSTANCE == null)
+			createInstance();
+		return INSTANCE;
+	}
+
+	/**
+	 * Crea la ventana
+	 */
+	public CargaAtributosWindow() {
+		initialize();
+		contenedor.setVisible(true);
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		contenedor = new JFrame();
+		contenedor.setTitle("Normalizator");
+		contenedor.setResizable(false);
+		contenedor.setBounds(100, 100, 650, 400);
+		contenedor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		contenedor.getContentPane().setLayout(null);
+
+		atributosRelacion = new ArrayList<String>();
+
+		JLabel lblCargaLosAtributos = new JLabel(
+				"Cargue los atributos de la relaci\u00F3n");
+		lblCargaLosAtributos.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCargaLosAtributos.setFont(new Font("Arial", lblCargaLosAtributos
+				.getFont().getStyle(), 18));
+		lblCargaLosAtributos.setBounds(145, 11, 324, 46);
+		contenedor.getContentPane().add(lblCargaLosAtributos);
+
+		JButton btnAtras = new JButton("Atr\u00E1s");
+		btnAtras.setFont(new Font("Arial", Font.PLAIN, 13));
+		contenedor.getContentPane().add(btnAtras);
+		btnAtras.setPreferredSize(new Dimension(110, 40));
+		btnAtras.setBounds(72, 325, 110, 29);
+		btnAtras.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				contenedor.setVisible(false);
+				NombreRelacionWindow nomRelWin = NombreRelacionWindow
+						.getInstance();
+				nomRelWin.setVisible(true);
+			}
+		});
+
+		JButton btnSiguiente = new JButton("Siguiente");
+		contenedor.getContentPane().add(btnSiguiente);
+		btnSiguiente.setPreferredSize(new Dimension(110, 40));
+		btnSiguiente.setBounds(449, 324, 110, 29);
+		btnSiguiente.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				Relacion.getInstance().setAtributos(atributosRelacion);
+				System.out.println(Relacion.getInstance().getAtributos());
+				setVisible(false);
+				DependenciasFuncionalesWindow.getInstance().setVisible(true);
+			}
+		});
+		txtfieldCargarAtributos = new JTextField();
+		txtfieldCargarAtributos.setSize(new Dimension(496, 35));
+		txtfieldCargarAtributos.setPreferredSize(new Dimension(500, 35));
+		txtfieldCargarAtributos.setMinimumSize(new Dimension(500, 20));
+		txtfieldCargarAtributos.setHorizontalAlignment(SwingConstants.CENTER);
+		txtfieldCargarAtributos.setColumns(35);
+		txtfieldCargarAtributos.setBounds(82, 68, 350, 35);
+		contenedor.getContentPane().add(txtfieldCargarAtributos);
+
+		JButton btnCargar = new JButton("Cargar");
+		btnCargar
+				.setFont(new Font("Arial", btnCargar.getFont().getStyle(), 14));
+		btnCargar.setBounds(431, 67, 95, 37);
+		btnCargar.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				atributo = txtfieldCargarAtributos.getText();
+				listaAtributos.add(atributo);
+				atributosRelacion.add(atributo);
+
+			}
+		});
+		contenedor.getContentPane().add(btnCargar);
+
+		listaAtributos = new List();
+		listaAtributos.setFont(new Font("Arial", Font.PLAIN, 12));
+		listaAtributos.setBounds(82, 115, 477, 159);
+		contenedor.getContentPane().add(listaAtributos);
+
+		btnBorrar = new JButton("Borrar");
+		btnBorrar.setBounds(72, 280, 497, 29);
+		contenedor.getContentPane().add(btnBorrar);
+		btnBorrar.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				int index = listaAtributos.getSelectedIndex();
+				listaAtributos.remove(index);
+				atributosRelacion.remove(index);
+			}
+		});
+
+	}
+
+	public void setVisible(boolean b) {
+		if (b == true)
+			contenedor.setVisible(true);
+		else
+			contenedor.setVisible(false);
+	}
+}
---
+++ Normalizator/src/ar/com/nn/view/VentanaPrincipal.java
@@ -0,0 +1,98 @@
+package ar.com.nn.view;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class VentanaPrincipal {
+
+	private static final int ANCHO_INTRO = 600;
+	private static final int ALTO_INTRO = 40;
+	private JFrame frmNormalizator;
+
+	private static VentanaPrincipal INSTANCE = null;
+
+	// creador sincronizado para protegerse de posibles problemas multi-hilo
+	// otra prueba para evitar instanciaci—n mœltiple
+	private synchronized static void createInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new VentanaPrincipal();
+		}
+	}
+
+	public static VentanaPrincipal getInstance() {
+		if (INSTANCE == null)
+			createInstance();
+		return INSTANCE;
+	}
+	
+	/**
+	 * Create the application.
+	 */
+	public VentanaPrincipal() {
+		initialize();
+		frmNormalizator.setVisible(true);
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmNormalizator = new JFrame();
+		frmNormalizator.setFont(new Font("Arial", Font.PLAIN, 12));
+		frmNormalizator.setResizable(false);
+		frmNormalizator.setTitle("Normalizator");
+		frmNormalizator.setBounds(100, 100, 650, 400);
+		frmNormalizator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel txtTitulo = new JLabel("Normalizator");
+		txtTitulo.setBounds(248, 51, 140, 30);
+		txtTitulo.setPreferredSize(new Dimension(130, 80));
+		txtTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		txtTitulo.setFont(new Font("Arial", txtTitulo.getFont().getStyle() | Font.BOLD, 21));
+		
+		JLabel txtIntro = new JLabel("Normaliz\u00E1 y calcul\u00E1 las distintas claves de tus relaciones");
+		txtIntro.setLocation(54, 136);
+		txtIntro.setPreferredSize(new Dimension(600, 170));
+		txtIntro.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+		txtIntro.setHorizontalAlignment(SwingConstants.CENTER);
+		txtIntro.setFont(new Font("Arial", txtIntro.getFont().getStyle(), 15));
+		txtIntro.setSize(530, 66);
+		
+		JButton btnComenzar = new JButton("Comenzar");
+		btnComenzar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				frmNormalizator.setVisible(false);
+				NombreRelacionWindow nombreRelWin = NombreRelacionWindow.getInstance();
+				nombreRelWin.setVisible(true);
+			}
+		});
+		btnComenzar.setBounds(260, 296, 128, 24);
+		btnComenzar.setPreferredSize(new Dimension(110, 40));
+		
+		JPanel panelAbsoluto = new JPanel();
+		frmNormalizator.getContentPane().add(panelAbsoluto, BorderLayout.CENTER);
+		panelAbsoluto.setLayout(null);
+		panelAbsoluto.add(txtTitulo);
+		panelAbsoluto.add(txtIntro);
+		panelAbsoluto.add(btnComenzar);
+	}
+
+	public void setVisible(boolean b) {
+		if (b == true)
+			frmNormalizator.setVisible(true);
+		else
+			frmNormalizator.setVisible(false);
+	}
+
+}
