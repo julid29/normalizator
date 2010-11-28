7553100c81ca22f6123385267ca9550b186a4f90
santiago.hollmann <santiago.hollmann@c02190c7-70d5-933a-f1a5-febd6bb59b4f>
2010-11-28 22:41:52 +0000

    Se agregaron los eventos para la tecla Enter en los textbox. Se hicieron mejor para volver a arrancar el programar.

--- Normalizator/src/ar/com/nn/view/NombreRelacionWindow.java
+++ Normalizator/src/ar/com/nn/view/NombreRelacionWindow.java
@@ -3,6 +3,8 @@ package ar.com.nn.view;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -21,7 +23,7 @@ public class NombreRelacionWindow {
 	private JTextField txtboxNombre;
 
 	private static NombreRelacionWindow INSTANCE = null;
-	
+
 	// creador sincronizado para protegerse de posibles problemas multi-hilo
 	// otra prueba para evitar instanciaci—n mœltiple
 	private synchronized static void createInstance() {
@@ -54,29 +56,45 @@ public class NombreRelacionWindow {
 		contenedor.getContentPane().setPreferredSize(new Dimension(200, 200));
 		contenedor.setBounds(100, 100, 650, 400);
 		contenedor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JPanel panelAbsoluto = new JPanel();
 		contenedor.getContentPane().add(panelAbsoluto, BorderLayout.CENTER);
 		panelAbsoluto.setLayout(null);
-		
-		JLabel lblIngreseNombre = new JLabel("Ingrese el nombre de su relaci\u00F3n");
+
+		JLabel lblIngreseNombre = new JLabel(
+				"Ingrese el nombre de su relaci\u00F3n");
 		lblIngreseNombre.setBounds(147, 56, 350, 35);
 		panelAbsoluto.add(lblIngreseNombre);
 		lblIngreseNombre.setPreferredSize(new Dimension(350, 35));
-		lblIngreseNombre.setFont(new Font("Arial", lblIngreseNombre.getFont().getStyle() | Font.BOLD, 22));
+		lblIngreseNombre.setFont(new Font("Arial", lblIngreseNombre.getFont()
+				.getStyle() | Font.BOLD, 22));
 		lblIngreseNombre.setHorizontalTextPosition(SwingConstants.CENTER);
 		lblIngreseNombre.setHorizontalAlignment(SwingConstants.CENTER);
-		
+
 		txtboxNombre = new JTextField();
 		txtboxNombre.setLocation(76, 170);
-		txtboxNombre.setFont(new Font("Arial", txtboxNombre.getFont().getStyle(), 18));
+		txtboxNombre.setFont(new Font("Arial", txtboxNombre.getFont()
+				.getStyle(), 18));
 		txtboxNombre.setMinimumSize(new Dimension(500, 20));
 		txtboxNombre.setPreferredSize(new Dimension(500, 35));
 		txtboxNombre.setSize(new Dimension(496, 35));
 		txtboxNombre.setHorizontalAlignment(SwingConstants.CENTER);
 		txtboxNombre.setColumns(35);
+		txtboxNombre.addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent e) {
+				int k = e.getKeyCode();
+				if (k == KeyEvent.VK_ENTER) {
+					Relacion rel = Relacion.getInstance();
+					rel.setNombre(txtboxNombre.getText());
+					setVisible(false);
+					CargaAtributosWindow cargaWin = CargaAtributosWindow
+							.getInstance();
+					cargaWin.setVisible(true);
+				}
+			}
+		});
 		panelAbsoluto.add(txtboxNombre);
-		
+
 		JButton btnAtras = new JButton("Atras");
 		btnAtras.setFont(new Font("Arial", btnAtras.getFont().getStyle(), 14));
 		btnAtras.setBounds(141, 313, 110, 40);
@@ -84,18 +102,20 @@ public class NombreRelacionWindow {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				contenedor.setVisible(false);
-				VentanaPrincipal ventanaPrincipalWin = VentanaPrincipal.getInstance();
+				VentanaPrincipal ventanaPrincipalWin = VentanaPrincipal
+						.getInstance();
 				ventanaPrincipalWin.setVisible(true);
 			}
 		});
-		
+
 		panelAbsoluto.add(btnAtras);
 		btnAtras.setPreferredSize(new Dimension(110, 40));
-		
+
 		JButton btnSiguiente = new JButton("Siguiente");
-		btnSiguiente.setFont(new Font("Arial", btnSiguiente.getFont().getStyle(), 14));
+		btnSiguiente.setFont(new Font("Arial", btnSiguiente.getFont()
+				.getStyle(), 14));
 		btnSiguiente.setBounds(387, 313, 110, 40);
-		
+
 		// Boton Siguiente
 		btnSiguiente.addMouseListener(new MouseAdapter() {
 			@Override
@@ -103,11 +123,12 @@ public class NombreRelacionWindow {
 				Relacion rel = Relacion.getInstance();
 				rel.setNombre(txtboxNombre.getText());
 				setVisible(false);
-				CargaAtributosWindow cargaWin = CargaAtributosWindow.getInstance();
+				CargaAtributosWindow cargaWin = CargaAtributosWindow
+						.getInstance();
 				cargaWin.setVisible(true);
 			}
 		});
-		
+
 		panelAbsoluto.add(btnSiguiente);
 		btnSiguiente.setPreferredSize(new Dimension(110, 40));
 	}
