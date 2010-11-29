c4fb5f422c679f23be078572a6f34d4e039ec822
nahuelul@gmail.com <nahuelul@gmail.com@c02190c7-70d5-933a-f1a5-febd6bb59b4f>
2010-11-29 10:38:39 +0000

    Borré las muestras por consola de los resultados. Ahora solo abre la GUI

--- trunk/Normalizator/src/ar/com/nn/busisness/Admin.java
+++ trunk/Normalizator/src/ar/com/nn/busisness/Admin.java
@@ -1,7 +1,5 @@
 package ar.com.nn.busisness;
 
-import java.util.ArrayList;
-
 import ar.com.nn.view.CargaAtributosWindow;
 import ar.com.nn.view.DatosRelacionWindow;
 import ar.com.nn.view.DependenciasFuncionalesWindow;
@@ -23,49 +21,7 @@ public class Admin {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		System.out.println("Bienvenido al aplicativo BD");
+	public static void main(String[] args){
 		VentanaPrincipal.getInstance();
-		ArrayList<String> atributos = new ArrayList<String>();
-		atributos.add("a");
-		atributos.add("b");
-		atributos.add("c");
-
-		
-		ArrayList<DepFuncional> depFuncionales = new ArrayList<DepFuncional>();
-		DepFuncional depFn = new DepFuncional();
-		depFn.setDeterminantes(atributos.get(0));
-		depFn.setDeterminados(atributos.get(1));
-		depFn.setDeterminados(atributos.get(2));
-		depFuncionales.add(depFn);
-		
-		Relacion r = new Relacion(1, "Nahuel",atributos, depFuncionales);
-		
-		System.out.println(r.getNombre());
-		System.out.println("Atributos:");
-		for (String a : r.getAtributos()) {
-			System.out.println(a);
-		}
-		r.obtenerFmin();
-		System.out.println("Fmin:");
-		for (DepFuncional depFuncional : r.getfMin()) {
-			for (String s : depFuncional.getDeterminantes()) {
-				System.out.println(s);
-			}
-			for (String s : depFuncional.getDeterminados()) {
-				System.out.println(s);
-			}
-		}
-		r.calcularClaves();
-		System.out.println(r.getFormaNormal());
-		r.calcular3FormaNormal();
-		System.out.println("3ra forma normal");
-		for (FormaNormal fn3 : r.getFormaNormal3()) {
-			System.out.println("Atributos:");
-			for(String a : fn3.getAtributos()){
-					System.out.println(a);
-			}
-		}
 	}
-
 }
